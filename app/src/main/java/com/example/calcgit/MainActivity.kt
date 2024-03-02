bfa4dd21aaf5d085292667a69bb47dd27ece9770
vlad hrabvchak <bigfutnk@gmail.com>
2024-03-02 18:28:22 +0100

    equals logik 1v

--- app/src/main/java/com/example/calcgit/MainActivity.kt
+++ app/src/main/java/com/example/calcgit/MainActivity.kt
@@ -32,12 +32,38 @@ class MainActivity : AppCompatActivity() {
         val divid = findViewById<Button>(R.id.divide)
         val coler = findViewById<Button>(R.id.cover)
         val coler2 = findViewById<Button>(R.id.cover2)
+        var lis = mutableListOf<String>()
+        var oo = ""
         var hh = ""
 
 
+
         egual.setOnClickListener(){
-            val gg: Int = hh.toInt()
-            text.text = gg.toString()
+            var oo = ""
+            var num1 = 0
+            var num2 = 0
+            var aa = false
+            for(gg in lis){
+                if(gg == "+" || gg == "-" || gg == "/" || gg == "*") {
+                    aa = !aa
+                    oo = ""
+                }
+                else {
+                    if (aa == false) {
+                        oo = oo + gg
+                        num1 = oo.toInt()
+                    } else {
+                        oo = oo + gg
+                        num2 = oo.toInt()
+                    }
+                }
+
+
+
+
+
+            }
+
         }
 
 
@@ -52,70 +78,87 @@ class MainActivity : AppCompatActivity() {
         nnum1.setOnClickListener(){
             hh = hh + "1"
             text.text = hh
+            lis.add("1")
         }
         nnum2.setOnClickListener(){
             hh = hh + "2"
             text.text = hh
+            lis.add("2")
         }
         nnum3.setOnClickListener(){
             hh = hh + "3"
             text.text = hh
+            lis.add("3")
         }
         nnum4.setOnClickListener(){
             hh = hh + "4"
             text.text = hh
+            lis.add("4")
         }
         nnum5.setOnClickListener(){
             hh = hh + "5"
             text.text = hh
+            lis.add("5")
         }
         nnum6.setOnClickListener(){
             hh = hh + "6"
             text.text = hh
+            lis.add("6")
         }
         nnum7.setOnClickListener(){
             hh = hh + "7"
             text.text = hh
+            lis.add("7")
         }
         nnum8.setOnClickListener(){
             hh = hh + "8"
              text.text = hh
+            lis.add("8")
         }
         nnum9.setOnClickListener(){
             hh = hh + "9"
             text.text = hh
+            lis.add("9")
         }
         nnum0.setOnClickListener(){
             hh = hh + "0"
             text.text = hh
+            lis.add("0")
         }
         dott.setOnClickListener(){
             hh = hh + "."
             text.text = hh
+            lis.add(".")
         }
         pls.setOnClickListener(){
             hh = hh + "+"
             text.text = hh
+            lis.add("+")
         }
         mns.setOnClickListener(){
             hh = hh + "-"
             text.text = hh
+            lis.add("-")
         }
         tims.setOnClickListener(){
             hh = hh + "*"
             text.text = hh
+            lis.add("*")
         }
         divid.setOnClickListener(){
             hh = hh + "/"
             text.text = hh
+            lis.add("/")
         }
         coler.setOnClickListener(){
             hh = hh + "("
             text.text = hh
+            lis.add("(")
         }
         coler2.setOnClickListener(){
             hh = hh +")"
             text.text = hh
+            lis.add(")")
         }
 
     }
