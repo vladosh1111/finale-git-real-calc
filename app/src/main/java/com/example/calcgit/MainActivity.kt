f17609e032444d1e975a64538f9f2bf0429da8d1
vlad hrabvchak <bigfutnk@gmail.com>
2024-03-10 17:13:42 +0100

    add logik for equals botton

--- app/src/main/java/com/example/calcgit/MainActivity.kt
+++ app/src/main/java/com/example/calcgit/MainActivity.kt
@@ -39,37 +39,71 @@ class MainActivity : AppCompatActivity() {
 
 
         egual.setOnClickListener(){
-            var oo = ""
+
+            //val hh: Array<String> = arrayOf("1", "3", "2", "4", "+", "4", "-", "3", "3", "*", "5", "8", "0", "-", "4", "5", "/", "4")
+            val jj = mutableListOf<String>()
+            val pp = mutableListOf<String>()
+            val oo = mutableListOf<String>()
+            var sigh1 = ""
+            var l = ""
             var num1 = 0
+            var nn = 0
+
             var num2 = 0
-            var aa = false
-            for(gg in lis){
-                if(gg == "+" || gg == "-" || gg == "/" || gg == "*") {
-                    aa = !aa
-                    oo = ""
+            pp.add("")
+            for (gg in lis) {
+                if (gg == "+" || gg == "-" || gg == "*" || gg == "/") {
+                    pp.add(gg)
+                    jj.add("|")
+                } else {
+                    jj.add(gg)
                 }
-                else {
-                    if (aa == false) {
-                        oo = oo + gg
-                        num1 = oo.toInt()
-                    } else {
-                        oo = oo + gg
-                        num2 = oo.toInt()
-                    }
-                }
-
-
-
 
 
             }
+            jj.add("|")
+            for (kk in jj) {
+                if (kk != "|") {
+                    l = l + kk
+                } else {
+                    oo.add(l)
+                    l = ""
+                }
+            }
+            for (aa in oo.indices){
+
+                // if(aa % 2 == 0){
+                num2 = oo[aa].toInt()
+                sigh1 = pp[aa]
+                when(sigh1){
+                    "+" -> num1 = num1 + num2
+                    "-" -> num1 = num1 - num2
+                    "*" -> num1 = num1 * num2
+                    "/" -> num1 = num1 / num2
+                    "" -> num1 = num1 + num2
+                }
+
+                num2 = 0
+                nn = nn + 1
+
+            }
+            val res: String = num1.toString()
+            text.text = res
+            lis.clear()
+            lis.add(res)
+
+
 
         }
+
+
+
 
 
         clear.setOnClickListener(){
             hh = ""
             text.text = ""
+
         }
 
         delete.setOnClickListener(){
